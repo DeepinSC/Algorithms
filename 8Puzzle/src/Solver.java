a2dabde74935aecd01c4a83e7e7931a19aba622e
Rick <654834341@qq.com>
2018-05-28 11:25:34 +0800

    Main template

--- 8Puzzle/src/Solver.java
+++ 8Puzzle/src/Solver.java
@@ -1,5 +1,8 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Solver {
-    public Solver(Board initial{
+    public Solver(Board initial){
 
     } // find a solution to the initial board (using the A* algorithm)
     public boolean isSolvable(){
@@ -11,7 +14,33 @@ public class Solver {
     public Iterable<Board> solution(){
 
     }// sequence of boards in a shortest solution; null if unsolvable
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-    }// solve a slider puzzle (given below)
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                blocks[i][j] = in.readInt();
+            }
+        }
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+            {
+                StdOut.println(board);
+            }
+        }
+    }
+
+// solve a slider puzzle (given below)
 }
